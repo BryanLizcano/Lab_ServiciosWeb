25fa0539338ea18ec99f7a9bf5863302578f9a85
Bryan Lizcano <bryanlizcano80@gmail.com>
2026-08-11 14:12:18 -0500

    Commit inicial

--- index.js
+++ index.js
@@ -4,26 +4,65 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-const categoriasAutos = [
+const autos = [
     {
-        nombre: 'JDM',
-        autos: ['Toyota Supra MK4', 'Nissan Skyline GT-R R34', 'Mazda RX-7 FD', 'Honda NSX', 'Subaru Impreza WRX STI']
+        id: 1,
+        marca: 'Toyota',
+        modelo: 'Supra MK4',
+        categoria: 'JDM',
+        existencias: 3,
+        proveedor: { nombre: 'Tokyo Motors Import', contacto: 'contacto@tokyomotors.jp' }
     },
     {
-        nombre: 'Americanos',
-        autos: ['Ford Mustang GT500', 'Chevrolet Camaro SS', 'Dodge Challenger Hellcat', 'Chevrolet Corvette Z06', 'Ford GT']
+        id: 2,
+        marca: 'Ford',
+        modelo: 'Mustang GT500',
+        categoria: 'Americano',
+        existencias: 0,
+        proveedor: { nombre: 'Detroit Auto Group', contacto: 'ventas@detroitauto.com' }
     },
     {
-        nombre: 'Alemanes',
-        autos: ['BMW M3 E46', 'Mercedes-AMG GT', 'Audi RS6 Avant', 'Porsche 911 GT3', 'Volkswagen Golf R']
+        id: 3,
+        marca: 'BMW',
+        modelo: 'M3 E46',
+        categoria: 'Alemán',
+        existencias: 7,
+        proveedor: { nombre: 'Munich Performance', contacto: 'info@munichperf.de' }
+    },
+    {
+        id: 4,
+        marca: 'Nissan',
+        modelo: 'Silvia S15',
+        categoria: 'Drifting',
+        existencias: 1,
+        proveedor: { nombre: 'Osaka Drift Parts', contacto: 'sales@osakadrift.jp' }
+    },
+    {
+        id: 5,
+        marca: 'Porsche',
+        modelo: '911 GT3',
+        categoria: 'Sport',
+        existencias: 5,
+        proveedor: { nombre: 'Stuttgart Classics', contacto: 'contact@stuttgartclassics.de' }
     },
 ];
 
 app.get('/', (req, res) => {
     res.render('index', {
         nombre: 'Bryan Lizcano',
-        categoriasAutos
+        autos: autos
     });
 });
+
+app.get('/auto/:id', (req, res) => {
+    const auto = autos.find(a => a.id === parseInt(req.params.id));
+
+    if (!auto) {
+        return res.status(404).send('Auto no encontrado');
+    }
+
+    res.render('detalle', { nombre: 'Bryan Lizcano', auto });
+});
+
 
 app.listen(3000, () => console.log('Servidor en http://localhost:3000'));
